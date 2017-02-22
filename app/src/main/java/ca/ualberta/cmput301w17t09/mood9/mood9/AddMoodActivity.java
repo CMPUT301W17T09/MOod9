0ac87a75d16150094e44aa480d36a30843a428d1
Fady Machaalani <fmachaal@cs.ualberta.ca>
2017-02-22 16:27:45 -0700

    added fragement view

---
+++ app/src/main/java/ca/ualberta/cmput301w17t09/mood9/mood9/AddMoodActivity.java
@@ -0,0 +1,13 @@
+package ca.ualberta.cmput301w17t09.mood9.mood9;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddMoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_mood);
+    }
+}
--- app/src/main/java/ca/ualberta/cmput301w17t09/mood9/mood9/FeedActivity.java
+++ app/src/main/java/ca/ualberta/cmput301w17t09/mood9/mood9/FeedActivity.java
@@ -1,13 +1,21 @@
 package ca.ualberta.cmput301w17t09.mood9.mood9;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
 public class FeedActivity extends AppCompatActivity {
+    //TODO add functionality to the search button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_feed, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +28,19 @@ public class FeedActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                addMood();
             }
         });
 
+
+        //Set the title of the toolbar - adjust it to each feed
         toolbar.setTitle(R.string.universal_feed);
 
+
+    }
+
+    private void addMood() {
+        Intent addMoodIntent = new Intent(FeedActivity.this, AddMoodActivity.class);
+        startActivity(addMoodIntent);
     }
 }
