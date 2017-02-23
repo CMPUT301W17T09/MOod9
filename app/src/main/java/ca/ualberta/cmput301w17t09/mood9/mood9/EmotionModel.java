a48ea305d323bd522f391cbe8408f1033e508dfc
Dannick Pomerleau <dannick@ualberta.ca>
2017-02-22 17:48:23 -0700

    Added Guava and ElasticSearch to project. Also made UserModel use LoadingCache and the other models to use ConcurrentHashMap

--- app/src/main/java/ca/ualberta/cmput301w17t09/mood9/mood9/EmotionModel.java
+++ app/src/main/java/ca/ualberta/cmput301w17t09/mood9/mood9/EmotionModel.java
@@ -1,23 +1,25 @@
 package ca.ualberta.cmput301w17t09.mood9.mood9;
 
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Created by dannick on 2/22/17.
  */
 
 public class EmotionModel {
-    protected static HashMap<String, Emotion> emotions;
+    protected static ConcurrentHashMap<String, Emotion> emotions;
 
     public static Emotion getEmotion(String id){
         return emotions.get(id);
     }
 
     public static void loadEmotions() {
-        EmotionModel.emotions = new HashMap<String, Emotion>();
+        EmotionModel.emotions = new ConcurrentHashMap<String, Emotion>();
     }
 
-    public static HashMap<String, Emotion> getEmotions(){
+    public static ConcurrentMap<String, Emotion> getEmotions(){
         return emotions;
     }
 }
--- app/src/main/java/ca/ualberta/cmput301w17t09/mood9/mood9/SocialSituationModel.java
+++ app/src/main/java/ca/ualberta/cmput301w17t09/mood9/mood9/SocialSituationModel.java
@@ -1,23 +1,24 @@
 package ca.ualberta.cmput301w17t09.mood9.mood9;
 
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by dannick on 2/22/17.
  */
 
 public class SocialSituationModel {
-    protected static HashMap<String, SocialSituation> socialSituations;
+    protected static ConcurrentHashMap<String, SocialSituation> socialSituations;
 
     public static SocialSituation getSocialSituation(String id){
         return socialSituations.get(id);
     }
 
     public static void loadSocialSituations() {
-        SocialSituationModel.socialSituations = new HashMap<String, SocialSituation>();
+        SocialSituationModel.socialSituations = new ConcurrentHashMap<String, SocialSituation>();
     }
 
-    public static HashMap<String, SocialSituation> getSocialSituations(){
+    public static ConcurrentHashMap<String, SocialSituation> getSocialSituations(){
         return socialSituations;
     }
 }
