cef93a45dd5a4290150fdab12015644782d3bbd5
Dannick Pomerleau <dannick@beta.priv.deuterium.ca>
2017-03-12 16:49:33 -0600

    Basic python script

--- test/src/python/test.py
+++ test/src/python/test.py
@@ -1,4 +1,19 @@
 import requests
+import json
+
+def to_json(obj):
+    return json.JSONEncoder().encode(obj)
+
 index_url = "http://cmput301.softwareprocess.es:8080/cmput301w17t09/"
-r.requests.get(index_url)
-print(str(r))
+mood_url = index_url + "mood9/_search?pretty=true"
+user_url = index_url + "user9/_search"
+user_search = {
+    "query" : {
+        "match_all" : {}
+    },
+}
+
+print(to_json(user_search))
+
+r = requests.get(user_url, data=to_json(user_search))
+print(str(r.json()))
