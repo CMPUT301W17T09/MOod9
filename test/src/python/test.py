7eb07d48098b3c235ada7c5ab57f39675b9020df
Dannick Pomerleau <dannick@beta.priv.deuterium.ca>
2017-03-12 22:58:19 -0600

    Check users and moods

--- test/src/python/test.py
+++ test/src/python/test.py
@@ -5,15 +5,15 @@ def to_json(obj):
     return json.JSONEncoder().encode(obj)
 
 index_url = "http://cmput301.softwareprocess.es:8080/cmput301w17t09/"
-mood_url = index_url + "mood9/_search?pretty=true"
+mood_url = index_url + "mood9/_search"
 user_url = index_url + "user9/_search"
-user_search = {
+search = {
     "query" : {
         "match_all" : {}
     },
 }
 
-print(to_json(user_search))
-
-r = requests.get(user_url, data=to_json(user_search))
-print(str(r.json()))
+r = requests.get(mood_url, data=to_json(search))
+print(str(json.dumps(r.json(), indent=2)))
+r = requests.get(user_url, data=to_json(search))
+print(str(json.dumps(r.json(), indent=2)))
