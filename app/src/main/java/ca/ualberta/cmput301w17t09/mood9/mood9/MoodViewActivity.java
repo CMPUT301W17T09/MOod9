c5d573393f7981042558da05d6dfc7c3b5c930dd
Dannick Pomerleau <dannick@ualberta.ca>
2017-03-20 16:18:54 -0600

    Added MoodView Layout

---
+++ app/src/main/java/ca/ualberta/cmput301w17t09/mood9/mood9/MoodViewActivity.java
@@ -0,0 +1,13 @@
+package ca.ualberta.cmput301w17t09.mood9.mood9;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoodViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mood_view);
+    }
+}
