aeb405f2e88d6ab52a847ba24d1b518dae84eb91
rohitvinnakota <rohitvinnakota@gmail.com>
2017-03-24 17:57:45 -0600

    more following

--- app/src/main/java/ca/ualberta/cmput301w17t09/mood9/mood9/ProfileActivity.java
+++ app/src/main/java/ca/ualberta/cmput301w17t09/mood9/mood9/ProfileActivity.java
@@ -27,20 +27,23 @@ public class ProfileActivity extends AppCompatActivity {
         String user_id = sharedPreferences.getString("user_id", null);
         TextView profileName = (TextView) findViewById(R.id.profile_name);
         ListView followees = (ListView) findViewById(R.id.followees);
+        ListView requests = (ListView) findViewById(R.id.requests);
+
         profileName.setText(name);
 
         User current_user = UserModel.getUserProfile(user_id);
 
-        ArrayList<String> followee_names = new ArrayList<String>();
-        for (String id : current_user.getFollowees()) {
-            User followee = UserModel.getUserProfile(id);
-            followee_names.add(followee.getName());
+
+        ArrayList<String> request_list = new ArrayList<String>();
+        for (String id : current_user.getRequests()) {
+            User requested = UserModel.getUserProfile(id);
+            request_list.add(requested.getName());
         }
 
-        ArrayAdapter<String> followees_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, followee_names);
+        ArrayAdapter<String> req_adapter = new ArrayAdapter<String>(this, R.layout.request_fragment, request_list);
+        //Profile now displays list of users the user is following
 
-        followees.setAdapter(followees_adapter);
-
+        requests.setAdapter(req_adapter);
 
 
     }
