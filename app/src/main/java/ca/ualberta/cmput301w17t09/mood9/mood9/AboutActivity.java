66640a3d02270a501b35710c9551a4dcb0568dd4
Dannick Pomerleau <dannick@ualberta.ca>
2017-03-24 19:52:41 -0600

    Added License into About Activity

--- app/src/main/java/ca/ualberta/cmput301w17t09/mood9/mood9/AboutActivity.java
+++ app/src/main/java/ca/ualberta/cmput301w17t09/mood9/mood9/AboutActivity.java
@@ -1,9 +1,15 @@
 package ca.ualberta.cmput301w17t09.mood9.mood9;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.InputStream;
+
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -11,7 +17,10 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        getSupportActionBar().setTitle("About");
+
         Button gotitButton = (Button) findViewById(R.id.gotit_button);
+        Button viewApacheButton = (Button) findViewById(R.id.view_apache2);
 
         gotitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -19,8 +28,14 @@ public class AboutActivity extends AppCompatActivity {
                 finish();
             }
         });
+        viewApacheButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.apache.org/licenses/LICENSE-2.0"));
+                startActivity(intent);
+            }
+        });
+
     }
-
-
-
 }
